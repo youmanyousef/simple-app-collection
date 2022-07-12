682aceaa877e55615275bd7aa1882466f231201b
youmanyousef <44135498+youmanyousef@users.noreply.github.com>
2022-07-11 21:54:34 -0700

    Working but unfinished img-to-pdf
    Working but lots of hardcoded stuff and magic variables. gonna tweak that out and add usability for unix-like systems. Also, have to work on a python only solution - system call to pdfjam is not that great.

---
+++ img-to-pdf.py
@@ -0,0 +1,48 @@
+# this is the windows version, this requires WSL
+# and a few packages on debian they are 
+# pdfjam from texlive-extra-utils
+
+from PIL import Image, UnidentifiedImageError
+from PyPDF2 import PdfFileMerger
+import sys
+import os
+
+files_dir = sys.argv[1]
+output = "final.pdf"
+papersize = "letter" # a4paper, letter, refer to https://github.com/rrthomas/pdfjam#documentation
+#------
+files = os.listdir(files_dir)
+outs = []
+merger = PdfFileMerger()
+print("Converting...")
+for file in files:
+    file = f'{files_dir}/{file}'
+    ## Validate images -- sanitize filenames later
+    try:
+        if file.split('.')[-1] == 'pdf':
+            print('pass!')
+            continue
+        with Image.open(file) as img:
+            filename = ''.join(file.split('.')[0])
+            (img.convert('RGB')).save(filename+'.pdf')
+            ##
+            # We need to find a better way to do this, convert to letter format.
+            ##
+            cmd = f'wsl pdfjam "{filename}.pdf" --outfile "{filename}_c.pdf" --paper {papersize}'
+            print(cmd)
+            os.system(cmd)
+            outs.append(f"{filename}.pdf")
+            outs.append(f"{filename}_c.pdf")
+            merger.append(f"{filename}_c.pdf")
+            ##
+            
+    except UnidentifiedImageError:
+        print(f"{file} is not an actual image file. Please input an actual file!")
+        exit()
+
+print("Merging...")
+merger.write(output)
+merger.close()
+print("Cleaning...")
+for file in outs:  
+    os.remove(file)
